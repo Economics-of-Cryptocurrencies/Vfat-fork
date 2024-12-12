80391061f87053f88e3f626948ff9078af1ecd7d
beautybliss-toml <beautybliss0508@gmail.com>
2024-12-12 16:39:32 +0300

    fix: slippage in swapleft

--- src/component/SwapLeft.tsx
+++ src/component/SwapLeft.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { LuBriefcaseBusiness } from "react-icons/lu";
 import SearchBox from "./ui/SearchBox";
 import network from "../tokendata/network";
 import token from "../tokendata/token";
@@ -8,6 +9,7 @@ function SwapLeft() {
   const [networkSelectedId, setNetworkSelectedId] = useState(-1);
   const [tokenSelectedId, setTokenSelectedId] = useState(-1);
   const [recieveTokenId, setReciveTokenId] = useState(0);
+  const [slippage, setSlippage] = useState(0.1);
   return (
     <div className="bg-card text-card-foreground rounded-lg border shadow sm:min-w-[450px]">
       <div className="flex flex-col space-y-1.5 p-6">
@@ -97,21 +99,21 @@ function SwapLeft() {
               data-tooltip-trigger=""
               type="button"
             >
-              <div className="text-muted-foreground flex text-xs items-center gap-2">
+              <div className="text-muted-foreground flex text-xs items-center gap-2 text-[var(--primary)]">
                 What is this?
               </div>
             </button>
           </div>
           <div className="flex gap-2">
             <div className="flex w-full">
-              <div className="border-input flex h-9 rounded-md border bg-background text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium has-[:focus]:ring-ring has-[:focus]:ring-1 w-full min-w-0 svelte-r7117t">
+              <div className="border-input flex h-9 border bg-background text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium has-[:focus]:ring-ring has-[:focus]:ring-1 w-full min-w-0 rounded-[2.8px]">
                 <input
-                  className="input w-full placeholder:text-muted-foreground focus-visible:outline-none bg-transparent px-3 py-1 disabled:cursor-not-allowed disabled:opacity-50 svelte-r7117t"
-                  id="slippage-input-874008ba-1c52-45a5-99e8-cc97b5d685e1"
+                  className="input w-full px-3 py-1 disabled:cursor-not-allowed disabled:opacity-50 focus-visible:outline-none"
                   min="0"
                   max="100"
                   step="0.1"
                   type="number"
+                  value={slippage}
                 />
               </div>
             </div>
@@ -119,32 +121,44 @@ function SwapLeft() {
               <button
                 type="button"
                 tabIndex={0}
-                className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm h-9 w-9 text-muted-foreground"
+                className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm h-9 w-9 text-muted-foreground text-[var(--primary)] rounded-[2.8px]"
                 data-button-root=""
+                onClick={() => {
+                  setSlippage(0.1);
+                }}
               >
                 0.1
               </button>
               <button
                 type="button"
                 tabIndex={0}
-                className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm h-9 w-9 text-muted-foreground"
+                className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm h-9 w-9 text-muted-foreground text-[var(--primary)] rounded-[2.8px]"
                 data-button-root=""
+                onClick={() => {
+                  setSlippage(0.5);
+                }}
               >
                 0.5
               </button>
               <button
                 type="button"
                 tabIndex={0}
-                className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm h-9 w-9 text-muted-foreground"
+                className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm h-9 w-9 text-muted-foreground text-[var(--primary)] rounded-[2.8px]"
                 data-button-root=""
+                onClick={() => {
+                  setSlippage(1);
+                }}
               >
                 1
               </button>
               <button
                 type="button"
                 tabIndex={0}
-                className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm h-9 w-9 text-muted-foreground"
+                className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm h-9 w-9 text-muted-foreground text-[var(--primary)] rounded-[2.8px]"
                 data-button-root=""
+                onClick={() => {
+                  setSlippage(3);
+                }}
               >
                 3
               </button>
@@ -157,15 +171,32 @@ function SwapLeft() {
         <button
           type="button"
           tabIndex={0}
-          className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 shadow h-9 px-4 py-2 gap-3"
-          disabled={false}
-          data-button-root=""
+          className="focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-[2.8px] text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 shadow h-9 px-4 py-2 gap-3 bg-[var(--greydark)] text-[var(--greywhite)]"
           style={{
             width: "100%",
             maxWidth: "unset",
             maxHeight: "48px",
           }}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide-icon lucide"
+          >
+            <path d="M6 10V5c0-1.7 1.3-3 3-3h1" key="13af7h"></path>
+            <path d="m3 7 3 3 3-3" key="x25e72"></path>
+            <circle cx="18" cy="6" r="4" key="14r987"></circle>
+            <path d="M18 14v5c0 1.7-1.3 3-3 3h-1" key="1ogxox"></path>
+            <path d="m21 17-3-3-3 3" key="1m4qyr"></path>
+            <circle cx="6" cy="18" r="4" key="jn0mx"></circle>
+          </svg>{" "}
           Swap
         </button>
       </div>
